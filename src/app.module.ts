3a88cbf77517f5582444e65d283fa54d1adb8689
Jordan Baron <jordannbaron@gmail.com>
2022-11-25 18:06:29 -0500

    add http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SearchController } from 'src/search/search.controller';
+import { SearchModule } from 'src/search/search.module';
 import { SearchService } from 'src/search/search.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [SearchModule, HttpModule],
   controllers: [AppController, SearchController],
   providers: [AppService, SearchService],
 })
