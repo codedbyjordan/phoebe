16e88e8d17e0dabed538e019d5b9d222565e422d
Jordan Baron <62375480+codedbyjordan@users.noreply.github.com>
2022-11-25 22:08:51 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '../src', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(3000, 0.0.0.0);
 }
 bootstrap();
