666a38ff18e3fa57feeeca06393a6091b8eaf10e
Jordan Baron <jordannbaron@gmail.com>
2023-12-17 22:39:31 -0500

    port search functionality

--- src/routes/search/index.ts
+++ src/routes/search/index.ts
@@ -1,3 +1,39 @@
+import { Context } from "elysia";
+import { JSDOM } from "jsdom";
+import { renderEjs } from "../../utils/render-ejs";
+
 export const get = {
-  handler: () => "Hello world!",
+  handler: async ({ query, set }: Context) => {
+    if (!query.q) set.redirect = "/";
+
+    const baseUrl = "https://html.duckduckgo.com/html?q=";
+    const res = await fetch(baseUrl + query.q);
+    const html = await res.text();
+
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+
+    const results = [];
+    const els = document.querySelectorAll(".links_main");
+
+    for (const el of els) {
+      const anchor = el.querySelector(".result__a") as HTMLAnchorElement;
+
+      const url = anchor.href.split(
+        /\/\/duckduckgo\.com\/l\/\?uddg\=|\?rut/
+      )[1];
+
+      const titleEncoded = new dom.window.DOMParser().parseFromString(
+        anchor.innerHTML,
+        "text/html"
+      );
+
+      results.push({
+        title: titleEncoded.documentElement.textContent,
+        url: decodeURIComponent(url),
+        description: document.querySelector(".result__snippet")?.innerHTML,
+      });
+    }
+    return renderEjs("search.ejs", { query: query.q, results });
+  },
 };
