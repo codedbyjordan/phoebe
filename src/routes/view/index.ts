71b938d410734f7fdb08ba86e0043c284bb816b3
Jordan Baron <jordannbaron@gmail.com>
2023-12-21 01:15:20 -0500

    view stub

--- src/routes/view/index.ts
+++ src/routes/view/index.ts
@@ -1,3 +1,11 @@
-export const get = {
-  handler: () => "Hello World!",
+import { Route } from "webster-router";
+
+export const get: Route = {
+  handler: async ({ query, set }) => {
+    if (!query.site) set.redirect = "/";
+
+    const siteToView = query.site!;
+    // use puppeteer or similar library to get the html of the site
+    // because if we just use fetch, then SPAs won't work
+  },
 };
