d6b1d4d7550dec1f19a0f5eaa27769a80a172051
Jordan Baron <jordannbaron@gmail.com>
2023-12-21 23:07:19 -0500

    basic viewing w/template..doesnt work very well

--- src/routes/view/index.ts
+++ src/routes/view/index.ts
@@ -1,11 +1,37 @@
+import { Readability } from "@mozilla/readability";
+import { t } from "elysia";
+import puppeteer from "puppeteer";
 import { Route } from "webster-router";
+import { JSDOM } from "jsdom";
+import { renderEjs } from "../../utils/render-ejs";
 
 export const get: Route = {
-  handler: async ({ query, set }) => {
-    if (!query.site) set.redirect = "/";
-
+  schema: {
+    query: t.Object({
+      site: t.String({
+        pattern: "^(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?$",
+      }),
+    }),
+  },
+  handler: async ({ query }) => {
     const siteToView = query.site!;
-    // use puppeteer or similar library to get the html of the site
-    // because if we just use fetch, then SPAs won't work
+
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+
+    await page.goto(siteToView);
+    const dom = await page.content();
+
+    const { document } = new JSDOM(dom).window;
+
+    const readable = new Readability(document as any).parse();
+
+    await browser.close();
+    return renderEjs("view.ejs", {
+      title: readable?.title,
+      content: readable?.content,
+    });
   },
 };
